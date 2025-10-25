260bb0776f3276e20675c47adbe30d26d570a6a7
Ranuthi Dahamsa <ranuthidahamsa12@gmail.com>
2025-10-26 00:45:11 +0530

    Added handleDeliveryRequest function

--- main.c
+++ main.c
@@ -9,6 +9,12 @@ char cities[MAX_CITIES][CITY_NAME_LENGTH];
 int cityCount = 0;
 int distances[MAX_CITIES][MAX_CITIES];
 
+char vehicleNames[3][30] = {"Van", "Truck", "Lorry"};
+int vehicleCapacity[3] = {1000, 5000, 10000};
+float vehicleRate[3] = {30.0, 40.0, 80.0};
+float vehicleSpeed[3] = {60.0, 50.0, 45.0};
+float vehicleFuelEfficiency[3] = {12.0, 6.0, 4.0};
+
 void manageCities();
 void manageDistances();
 void handleDeliveryRequest();
@@ -345,3 +351,66 @@ void displayDistanceTable()
         printf("\n");
     }
 }
+
+void handleDeliveryRequest()
+{
+    if (cityCount < 2)
+    {
+        printf("Need at least 2 cities for delivery!\n");
+        return;
+    }
+
+    displayCities();
+
+    int source, destination, weight, vehicleType;
+
+    printf("Enter source city number: ");
+    scanf("%d", &source);
+    printf("Enter destination city number: ");
+    scanf("%d", &destination);
+
+    if (source == destination)
+    {
+        printf("Source and destination cannot be the same!\n");
+        return;
+    }
+
+    source--;
+    destination--;
+
+    printf("Enter weight(kg): ");
+    scanf("%d", &weight);
+
+    if (weight <= 0)
+    {
+        printf("Weight must be positive!\n");
+        return;
+    }
+
+    printf("\n===== Available Vehicles =====\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf(" %d. %-10s - Capacity: %5d kg, Rate: %.0f LKR/km\n", i + 1, vehicleNames[i], vehicleCapacity[i], vehicleRate[i]);
+    }
+
+    printf("Select vehicle type (1-3): ");
+    scanf("%d", &vehicleType);
+
+    if (vehicleType < 1 || vehicleType > 3)
+    {
+        printf("Invalid. Enter again");
+        return;
+    }
+
+    vehicleType--;
+
+    if (weight > vehicleCapacity[vehicleType])
+    {
+        printf("Weight exceeds vehicle capacity!\n");
+        printf("Maximum capacity for %s: %d kg\n", vehicleNames[vehicleType], vehicleCapacity[vehicleType]);
+        return;
+    }
+
+}
+
